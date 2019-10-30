164e8c4e5cc140b9261350e37a0711ecb24aeb35
imrankhanswati <imrankhanswati315@gmail.com>
2019-10-29 20:18:38 -0700

    Admob Integrated

---
+++ Assets/Scripts/Ads Controllers/AdsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AdmobControllerScript))]
+public class AdsController : MonoBehaviour
+{
+    public static AdsController instance;
+
+    private AdmobControllerScript admob;
+
+    private void Awake()
+    {
+        instance = this;
+        admob = GetComponent<AdmobControllerScript>();
+    }
+
+    public void ShowBanner()
+    {
+        admob.RequestBanner();
+    }
+
+    public void HideBanner()
+    {
+        admob.HideBanner();
+    }
+
+    public void ShowIntertitial()
+    {
+        if (admob.IsIntersititialAdLoaded())
+        {
+            admob.ShowInterstitial();
+        }
+    }
+}
