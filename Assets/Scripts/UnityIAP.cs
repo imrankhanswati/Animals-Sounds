45c76bf28b2280aa77de41388c593356748bb4d1
imrankhanswati <imrankhanswati315@gmail.com>
2019-11-20 08:50:02 +0500

    IAP Integrated_Not tested

---
+++ Assets/Scripts/UnityIAP.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class UnityIAP : MonoBehaviour, IStoreListener
+{
+    public static UnityIAP instance;
+
+    private static IStoreController m_StoreController;
+    private static IExtensionProvider m_StoreExtensionProvider;
+
+    public string no_ads;
+    public string buyCoffe;
+    public string buyLunch;
+    public string buyDinner;
+    public string fullDayOnMe;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        if (m_StoreController == null)
+        {
+            InitializePurchasing();
+        }
+    }
+
+    public void InitializePurchasing()
+    {
+        if (IsInitialized())
+        {
+            return;
+        }
+
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+
+        builder.AddProduct(no_ads, ProductType.Consumable);
+        builder.AddProduct(buyCoffe, ProductType.Consumable);
+        builder.AddProduct(buyLunch, ProductType.Consumable);
+        builder.AddProduct(buyDinner, ProductType.Consumable);
+        builder.AddProduct(fullDayOnMe, ProductType.Consumable);
+
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+
+    private bool IsInitialized()
+    {
+        return m_StoreController != null && m_StoreExtensionProvider != null;
+    }
+
+    public void Buy_NoAds_Pakage()
+    {
+        BuyProductID(no_ads);
+    }
+    public void Buy_Coffe_Pakage()
+    {
+        BuyProductID(buyCoffe);
+    }
+    public void Buy_Lunch_Pakage()
+    {
+        BuyProductID(buyLunch);
+    }
+    public void Buy_Dinner_Pakage()
+    {
+        BuyProductID(buyDinner);
+    }
+    public void Buy_FullDay_Pakage()
+    {
+        BuyProductID(fullDayOnMe);
+    }
+
+    void BuyProductID(string productId)
+    {
+        if (IsInitialized())
+        {
+            Product product = m_StoreController.products.WithID(productId);
+
+            if (product != null && product.availableToPurchase)
+            {
+                Debug.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));
+                m_StoreController.InitiatePurchase(product);
+            }
+            else
+            {
+                Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+            }
+        }
+        else
+        {
+            Debug.Log("BuyProductID FAIL. Not initialized.");
+        }
+    }
+
+
+    public void RestorePurchases()
+    {
+        if (!IsInitialized())
+        {
+            Debug.Log("RestorePurchases FAIL. Not initialized.");
+            return;
+        }
+
+        if (Application.platform == RuntimePlatform.IPhonePlayer ||
+            Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            Debug.Log("RestorePurchases started ...");
+
+            var apple = m_StoreExtensionProvider.GetExtension<IAppleExtensions>();
+
+            apple.RestoreTransactions((result) =>
+            {
+                Debug.Log("RestorePurchases continuing: " + result + ". If no further messages, no purchases available to restore.");
+            });
+        }
+        else
+        {
+            Debug.Log("RestorePurchases FAIL. Not supported on this platform. Current = " + Application.platform);
+        }
+    }
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        Debug.Log("OnInitialized: PASS");
+
+        m_StoreController = controller;
+        m_StoreExtensionProvider = extensions;
+    }
+
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        Debug.Log("OnInitializeFailed InitializationFailureReason:" + error);
+    }
+
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        if (String.Equals(args.purchasedProduct.definition.id, no_ads, StringComparison.Ordinal))
+        {
+            Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+            Debug.Log("IAP Success, NoAds");
+        }
+        else if (String.Equals(args.purchasedProduct.definition.id, buyCoffe, StringComparison.Ordinal))
+        {
+            Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+            Debug.Log("IAP Success, coffe");
+        }
+        else if (String.Equals(args.purchasedProduct.definition.id, buyLunch, StringComparison.Ordinal))
+        {
+            Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+            Debug.Log("IAP Success, lunch");
+        }
+        else if (String.Equals(args.purchasedProduct.definition.id, buyDinner, StringComparison.Ordinal))
+        {
+            Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+            Debug.Log("IAP Success, dinner");
+        }
+        else if (String.Equals(args.purchasedProduct.definition.id, fullDayOnMe, StringComparison.Ordinal))
+        {
+            Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+            Debug.Log("IAP Success, full day on me");
+        }
+        else
+        {
+            Debug.Log(string.Format("ProcessPurchase: FAIL. Unrecognized product: '{0}'", args.purchasedProduct.definition.id));
+        }
+
+        return PurchaseProcessingResult.Complete;
+    }
+
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
+    }
+}
